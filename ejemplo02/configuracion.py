c8ef872a542773bd7b6ef7978e164ba0de39f2d1
Jhoselin Guachizaca <jaguachizaca@gmail.com>
2022-05-31 15:54:26 -0500

    taller

---
+++ ejemplo02/configuracion.py
@@ -0,0 +1,11 @@
+# este módulo será usado para posibles configuraciones
+#
+# cadena conector a la base de datos
+#
+# Sqlite
+cadena_base_datos  =  'sqlite:///dataTaller07.db' 
+
+# mysql
+# para el uso de este dialecto en SqlAlchemy
+# instalar "pip instalar PyMySQL"
+#cadena_base_datos = 'mysql+mysqlconnector://salai:UTPLUTPL@localhost:3306/dataTaller07'
